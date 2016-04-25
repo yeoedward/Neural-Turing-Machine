25302a1ba8f2bf9c1071f55cf141b55d81f7adf8
Eddy Yeo <eddy.yeo@gmail.com>
2016-04-25 15:35:08 -0400

    Tested and fixed rotation op

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 15-453 Final Project
 
-Implementation of a Neural Turing Machine (NTM) using Tensor Flow
+Implementation of a Neural Turing Machine (NTM) using Tensor Flow (v0.8)
 
 Paper: http://arxiv.org/abs/1410.5401
 
 TODO:
 
-  Test NTM Memory mechanism.
+  Test NTM addressing mechanism.
 
   Implement controller.
 
@@ -31,6 +31,10 @@ Copy Task:
       graphs. In particular, the tf.split/tf.unpack functions require
       the number of steps as a python integer (i.e. we need to know it
       when we're building the graph).
+
+Installation:
+
+sudo pip install --upgrade https://storage.googleapis.com/tensorflow/mac/tensorflow-0.8.0-py2-none-any.whl
 
 Citations:
 
--- ntm.py
+++ ntm.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow.models.rnn import rnn_cell
 # WARNING: This is not part of the public API and might break 
 # in future versions of tensorflow.
-from tensorflow.python.ops import tensor_array_ops
+from tensorflow.python.ops import control_flow_ops, tensor_array_ops
 
 class NTMCell(rnn_cell.RNNCell):
   def __init__(self, mem_nrows, mem_ncols):
@@ -20,47 +20,52 @@ class NTMCell(rnn_cell.RNNCell):
   def controller(self, inputs, r):
     raise Exception("Not implemented yet")
 
+  @staticmethod
   def magnitude(tens, dim):
     return tf.sqrt(tf.reduce_sum(tf.square(tens), dim))
 
+  @staticmethod
   def conv_sum(wg_i, shift_i, row_idx):
     i = tf.constant(0)
     total = tf.constant(0)
     def conv_step(i, wg_i, shift_i, row_idx, total): 
-      si = (row_idx - i) % tf.shape(shift_i)[0]
-      total += wg_i[i] * shift_i[si]
+      shift_len = tf.shape(shift_i)[0]
+      si = (row_idx - i) % shift_len
+      si = tf.expand_dims((si + shift_len) % shift_len, 0)
+      total += (tf.slice(wg_i, tf.expand_dims(i, 0), [1]) *
+        tf.slice(shift_i, si, [1]))
       i += 1  
       return i, wg_i, shift_i, row_idx, total
-    (_, _, _, _, total) = tf.while_loop(
+    (_, _, _, _, total) = control_flow_ops.while_loop(
       cond=lambda i, wg_i, _1, _2, _3: i < tf.shape(wg_i)[0],
       body=conv_step,
       loop_vars=(i, wg_i, shift_i, row_idx, total),
     )
     return total
 
+  @staticmethod
   def rotate(wg, shift):
     # WARNING: Not in public API.
     ws = tensor_array_ops.TensorArray(
       dtype=wg.dtype,
       size=tf.reduce_prod(tf.shape(wg)),
     )
-    i = tf.constant(0)
     def rotate_step(i, wg, shift, ws):
-      batch_idx = i / tf.shape(wg)[0]
-      row_idx = i % tf.shape(wg)[0]
-      wg_i = tf.slice(wg, [batch_idx, 0], [1, -1])
-      shift_i = tf.slice(shift, [batch_idx, 0], [1, -1])
-      ws_i = conv_sum(wg_i, shift_i, row_idx)
-      ws.write(i, ws_i)
+      batch_idx = i / tf.shape(wg)[1]
+      row_idx = i % tf.shape(wg)[1]
+      wg_i = tf.squeeze(tf.slice(wg, tf.pack([batch_idx, 0]), [1, -1]))
+      shift_i = tf.squeeze(tf.slice(shift, tf.pack([batch_idx, 0]), [1, -1]))
+      ws_i = NTMCell.conv_sum(wg_i, shift_i, row_idx)
+      ws = ws.write(i, ws_i)
       i += 1
       return i, wg, shift, ws
     i = tf.constant(0)
-    (_, _, _, ws) = tf.while_loop(
+    (_, _, _, ws) = control_flow_ops.while_loop(
       cond=lambda i, wg, _1, _2: i < tf.reduce_prod(tf.shape(wg)),
       body=rotate_step,
       loop_vars=(i, wg, shift, ws),
     )
-    ws = tf.reshape(ws.pack(), [tf.shape(wg)[0], tf.shape(wg)[1]])
+    ws = tf.reshape(ws.pack(), tf.pack([tf.shape(wg)[0], tf.shape(wg)[1]]))
     return ws
 
   def __call__(self, inputs, state, scope=None):
