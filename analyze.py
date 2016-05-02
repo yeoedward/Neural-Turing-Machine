d45349c6d05b836e330654449d3b83791347d143
Eddy Yeo <eddy.yeo@gmail.com>
2016-05-02 03:13:36 -0400

    Generated input/output images. Organized folders

--- analyze.py
+++ analyze.py
@@ -1,33 +1,33 @@
-# Analyze specfic test case
-
+# Print bitmaps of inputs and outputs for sequences of various length.
+# For the report.
+import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
-from copy_task import model
+from copy_task import model, gen_seq
 from ntm import NTMCell
+
+test_lens = [5, 10, 30, 40, 50]
+inputs = []
+outputs = []
+nsteps = []
+for seq_len in test_lens:
+  max_steps = seq_len*2 + 1
+  x, y, steps = gen_seq(1, max_steps, seq_len, 3)
+  inputs.append(x)
+  outputs.append(y)
+  nsteps.append(steps)
 
 saver = tf.train.Saver()
 with tf.Session() as sess:
   # Load saved model
-  saver.restore(sess, "test/model41000.ckpt")
-  # 2675435 in binary.
-  xs = [[
-    [0, 1, 0],
-    [1, 1, 0],
-    [1, 1, 1],
-    [1, 0, 1],
-    [1, 0, 0],
-    [0, 1, 1],
-    [1, 0, 1],
-    [0, 0, 1], # GO
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-  ]]
-  nsteps = [15]
-  print sess.run(model["pred"], feed_dict={
-    model['x']: xs,
-    model['steps']: nsteps,
-  })
+  saver.restore(sess, "copy_task_checkpoints/model41000.ckpt")
+  for i in xrange(len(inputs)):
+    output = sess.run(model["pred"], feed_dict={
+      model['x']: inputs[i],
+      model['steps']: nsteps[i],
+    })
+    seq_len = test_lens[i]
+    plt.matshow(np.transpose(inputs[i][0][:seq_len]))
+    plt.savefig('images/inputs%d.png' % seq_len, dpi=100)
+    plt.matshow(np.transpose(output[0]))
+    plt.savefig('images/outputs%d.png' % seq_len, dpi=100)
--- plot.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import data
-
-plt.matshow(data.read)
-plt.matshow(data.write)
-plt.matshow(data.inputs)
-plt.matshow(data.prediction)
-plt.show()
